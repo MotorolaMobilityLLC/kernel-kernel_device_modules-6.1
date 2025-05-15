486d4f7dde5f23b590badf048d1173f2e00d32dc
lalit.w <lalit.w@mediatek.com>
2025-05-15 12:19:07 +0800

    disp: dp: modify dual pipe conditions
    [Description]
    modify dual pipe conditions
    
    [Test]
    Boot Pass
    Customer Verify Pass
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I8c31d005200157d254cff9dec05304d32b01242b
    Signed-off-by: lalit.w <lalit.w@mediatek.com>
    CR-Id: ALPS09863212
    Feature: display_mobile
    Reviewed-on: https://gerrit.mot.com/3395182
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 2b372319f4762b5dd270ab292fbd9a97d29b21ad)
    Reviewed-on: https://gerrit.mot.com/3417009
    Reviewed-by: Cong Wang <wangc22@motorola.com>
    Submit-Approved: Cong Wang <wangc22@motorola.com>
    Tested-by: Cong Wang <wangc22@motorola.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -2523,6 +2523,15 @@ bool mtk_crtc_is_dual_pipe(struct drm_crtc *crtc)
 	if (drm_crtc_index(crtc) == 1) {
 		if (priv->data->mmsys_id == MMSYS_MT6989)
 			return false;
+
+		if (priv->data->mmsys_id == MMSYS_MT6897 &&
+			((drm_mode_vrefresh(&crtc->state->adjusted_mode) == 120) ||
+			(crtc->state->adjusted_mode.clock >= MTK_PCLK_2K60) ||
+			(crtc->state->adjusted_mode.hdisplay > 1952))) {
+			DDPFUNC();
+			return true;
+		}
+
 		if ((crtc->state->adjusted_mode.hdisplay == 1920*2) ||
 			(drm_mode_vrefresh(&crtc->state->adjusted_mode) == 120) ||
 			(crtc->state->adjusted_mode.clock >= MTK_PCLK_2K60)) {
